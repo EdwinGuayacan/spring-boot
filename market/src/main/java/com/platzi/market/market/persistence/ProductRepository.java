d9fb07bd101caca3ef9532668e0c91179e74d586
EdwinGuayacan <80716239+EdwinGuayacan@users.noreply.github.com>
2024-01-31 15:00:41 -0500

    query methods and repository tag

--- market/src/main/java/com/platzi/market/market/persistence/ProductRepository.java
+++ market/src/main/java/com/platzi/market/market/persistence/ProductRepository.java
@@ -2,12 +2,34 @@ package com.platzi.market.market.persistence;
 
 import com.platzi.market.market.persistence.crud.ProductCrudRepository;
 import com.platzi.market.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+import java.util.Optional;
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
     public List<Product> getAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory (int categoryId){
+        return  productCrudRepository.findByCategoryIdOrderByNameASC(categoryId);
+    }
+
+    public Optional<List<Product>> getLimited(int amount){
+        return productCrudRepository.findByStockLessThanAndStatus(amount, true);
+    }
+
+    public Optional<Product> getProduct(int productId){
+        return productCrudRepository.findById(productId);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int productId) {
+        productCrudRepository.deleteById(productId);
     }
 }
--- market/src/main/java/com/platzi/market/market/persistence/crud/ProductCrudRepository.java
+++ market/src/main/java/com/platzi/market/market/persistence/crud/ProductCrudRepository.java
@@ -3,6 +3,10 @@ package com.platzi.market.market.persistence.crud;
 import com.platzi.market.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByCategoryIdOrderByNameASC(int categoryId);
+    Optional<List<Product>> findByStockLessThanAndStatus(int stock, boolean status);
 }
